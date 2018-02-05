562404ab30d9b837035f6494359406dd501780b2
Erik Landvall <erik@landvall.se>
2018-02-05 17:11:07 +0100

    -.-

--- server/http.js
+++ server/http.js
@@ -60,7 +60,7 @@ module.exports = class extends require('./_abstract')
                status  : 'failed',
                message : 'unhandled exception' } }
     }
-    
+
     const
     View    = await this.fetchView(vm.view || route.view),
     output  = await new View.compose(vm, route)
