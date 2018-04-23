3152df051ca64c7126c2f229bc4244bdb7f26a80
Erik Landvall <erik@landvall.se>
2018-04-23 20:47:47 +0200

    Added additional mime types to the resource dispatcher

--- controller/dispatcher/resource.js
+++ controller/dispatcher/resource.js
@@ -23,21 +23,26 @@ module.exports = class extends Dispatcher
     try
     {
       const
+      CT        = 'Content-Type',
       headers   = {},
       resource  = base + origin + path.normalize(this.request.url.pathname),
       extension = path.extname(resource).toLowerCase(),
       source    = resource in cached
-                ? cached[resource]
-                : cached[resource] = await readFile(resource, 'utf-8')
+                  ? cached[resource]
+                  : cached[resource] = await readFile(resource, 'utf-8')
 
       switch(extension)
       {
-        case 'js'   : headers['Content-Type'] = 'application/javascript' break
-        case 'css'  : headers['Content-Type'] = 'text/css'    break
-        case 'jpg'  :
-        case 'jpeg' : headers['Content-Type'] = 'image/jpeg'  break
-        case 'gif'  : headers['Content-Type'] = 'image/gif'   break
-        case 'png'  : headers['Content-Type'] = 'image/png'   break
+        case '.jpg'   :
+        case '.jpeg'  : headers[CT] = 'image/jpeg'  break
+        case '.gif'   : headers[CT] = 'image/gif'   break
+        case '.png'   : headers[CT] = 'image/png'   break
+        case '.css'   : headers[CT] = 'text/css'    break
+        case '.csv'   : headers[CT] = 'text/csv'    break
+        case '.pdf'   : headers[CT] = 'application/pdf'           break
+        case '.json'  : headers[CT] = 'application/json'          break
+        case '.ico'   : headers[CT] = 'image/vnd.microsoft.icon'  break
+        case '.js'    : headers[CT] = 'application/javascript'    break
       }
 
       return { headers, body : source }
