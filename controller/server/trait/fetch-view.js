0e0d75291bd745e7d27b022eeb247c67e37744ff
Erik Landvall <erik@landvall.se>
2018-04-25 16:34:29 +0200

    Added test for the fetch view trait

--- controller/server/trait/fetch-view.js
+++ controller/server/trait/fetch-view.js
@@ -4,10 +4,8 @@ path = require('path'),
 root = path.dirname(require.main.filename)
 
 module.exports = (view) =>
-{
-  return view && fs.existsSync(`${root}/${view}.js`)
+  view && fs.existsSync(`${root}/${view}.js`)
   ? require.main.require(view)
   : view && fs.existsSync(`${__dirname}/../../../view/${view}.js`)
     ? require(`../../../view/${view}`)
     : require(`../../../view/json`)
-}
