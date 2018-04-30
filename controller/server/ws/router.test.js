ad20fd98193e115724c5aa3638b208922a291d93
Erik Landvall <erik@landvall.se>
2018-04-30 17:26:30 +0200

    Indentations

--- controller/server/ws/router.test.js
+++ controller/server/ws/router.test.js
@@ -28,13 +28,13 @@ describe('controller/server/ws/router', () =>
     result2 = router.findRoute('bazqux'),
     result3 = router.findRoute('no-matching-policy')
 
-    it('correct dispatcher found', () =>
-      expect(result1.dispatcher).to.be.equal('foo'))
+    it('correct dispatcher found',
+    () => expect(result1.dispatcher).to.be.equal('foo'))
 
-    it('regex policy', () =>
-      expect(result2.dispatcher).to.be.equal('baz'))
+    it('regex policy',
+    () => expect(result2.dispatcher).to.be.equal('baz'))
 
-    it('no matching policy should return an undefined dispatcher', () =>
-      expect(result3.dispatcher).to.be.equal(undefined))
+    it('no matching policy should return an undefined dispatcher',
+    () => expect(result3.dispatcher).to.be.equal(undefined))
   })
 })
