316e0af60b62986667353bb224d006686dc4ed65
Erik Landvall <erik@landvall.se>
2018-05-02 18:27:52 +0200

    Expanded the the test suit for the "toFixed" helper

--- view/template/helper/toFixed.test.js
+++ view/template/helper/toFixed.test.js
@@ -4,6 +4,21 @@ describe('view/template/helper/toFixed', () =>
 {
   const toFixed = require('./toFixed')
 
-  it('should return a padded number', () =>
-    expect(toFixed(5, 3)).to.be.equal('5.000'))
+  it('should return a padded number',
+  () => expect(toFixed(5, 2)).to.be.equal('5.00'))
+
+  it('should be able to handle "null"',
+  () => expect(toFixed(null, 2)).to.be.equal('0.00'))
+
+  it('should return NaN if "undefined" is spcified',
+  () => expect(toFixed(undefined, 2)).to.be.equal('NaN'))
+
+  it('should be able to handle negative numbers',
+  () => expect(toFixed(-5, 2)).to.be.equal('-5.00'))
+
+  it('should be able to handle strings',
+  () => expect(toFixed('5', 2)).to.be.equal('5.00'))
+
+  it('should be able to handle strings that is not formatted as a number',
+  () => expect(toFixed('foobar', 2)).to.be.equal('NaN'))
 })
