47b8f05479ee46cf1ee261305cf515e21bd8654b
Erik Landvall <erik@landvall.se>
2018-05-02 14:11:17 +0200

    Set the "dateformat" module as an optional module only required if the helper dependent on the module is being used

--- view/template/helper/date.js
+++ view/template/helper/date.js
@@ -1,2 +1,17 @@
-const dateformat = require('dateformat')
+const dateformat = (() =>
+{
+  try
+  {
+    // "dateformat" is an optional dependency
+    // but required for this helper to work
+    return require('dateformat')
+  }
+  catch(err)
+  {
+    const error = new Error('Missing required module "dateformat"')
+    error.code  = 'ERR_MISSING_MODULE'
+    throw error
+  }
+})()
+
 module.exports = (date, format) => dateformat(date, format)
