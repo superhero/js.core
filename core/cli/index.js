6c67f9489e72b48d78daa5f1edb038d8ccd27ccf
Erik Landvall <erik@landvall.se>
2019-07-01 14:43:57 +0200

    Updated the CLI component

--- core/cli/index.js
+++ core/cli/index.js
@@ -1,20 +1,28 @@
-class Cli
+const Debug = require('@superhero/debug')
+
+class Cli extends Debug
 {
   /**
    * @param {readline} readline
-   * @param {string} prompt
    */
-  constructor(readline, prompt)
+  constructor(readline)
   {
+    const options =
+    {
+      date            : false,
+      maxStringLength : 0,
+      separator       : '\n'
+    }
+    super(options)
     this.readline = readline
-    this.prompt   = prompt
   }
 
   /**
    * @param {string} question
-   * @param {function} completer @see https://nodejs.org/api/readline.html#readline_use_of_the_completer_function
+   * @param {string} alternatives
+   * @see https://nodejs.org/api/readline.html#readline_use_of_the_completer_function
    */
-  question(question, completer)
+  question(question, alternatives)
   {
     return new Promise((accept, reject) =>
     {
@@ -23,26 +31,61 @@ class Cli
         prompt    : this.prompt,
         input     : process.stdin,
         output    : process.stdout,
-        completer
+        completer : Array.isArray(alternatives) === false
+                  ? undefined
+                  : (line) =>
+                    {
+                      const completions = alternatives.filter((c) => c.startsWith(line))
+                      return [ completions.length ? completions : alternatives, line ]
+                    }
       })
 
-      rl.question(question, (input) =>
+      rl.question(question + ' ', async (input) =>
       {
         rl.close()
-        accept(input)
+
+        if(Array.isArray(alternatives) && alternatives.length)
+        {
+          if(alternatives.includes(input))
+          {
+            accept(input)
+          }
+          else
+          {
+            if(alternatives.length === 1)
+            {
+              await this.write(`You must chose ${alternatives}`, 'red')
+            }
+            else if(alternatives.length < 10)
+            {
+              await this.write(`You must chose one of the alternatives ${alternatives.slice(0, -1).join(', ')} or ${alternatives.slice(-1)}`, 'red')
+            }
+            else
+            {
+              await this.write(`You must chose one of the ${alternatives.length} valid alternatives`, 'red')
+              await this.write(`You can use the "tab" key to list all the alternatives `, 'red')
+            }
+
+            const answer = await this.question(question, alternatives)
+            accept(answer)
+          }
+        }
+        else
+        {
+          accept(input)
+        }
       })
     })
   }
 
   /**
-   * @param {string|Buffer|Uint8Array|any}  chunk     not null...
-   * @param {string}                        encoding  utf8
+   * @param {*} chunk
+   * @param {string} color
    */
-  write(chunk, encoding = 'utf8')
+  write(chunk, color)
   {
-    return new Promise((accept) =>
-         process.stdout.write(chunk, encoding, () => accept())
-      || process.stdout.emit('drain'))
+    this.color(color).log(chunk)
+    this.color(false)
   }
 }
 
--- core/cli/locator.js
+++ core/cli/locator.js
@@ -4,18 +4,9 @@ readline  = require('readline')
 
 class CliLocator
 {
-  constructor(locator)
-  {
-    this.locator = locator
-  }
-
   locate()
   {
-    const
-    configuration = this.locator.locate('core/configuration'),
-    prompt        = configuration.find('core.cli.prompt')
-
-    return new Cli(readline, prompt)
+    return new Cli(readline)
   }
 }
 
