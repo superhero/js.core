d14c9172ed6471b43c467aa5b67b679b837e9a13
arsu-leo <arsu.leo@gmail.com>
2019-08-22 14:38:29 +0200

    Added remove object property functionality

--- core/object/index.js
+++ core/object/index.js
@@ -14,6 +14,27 @@ class CoreObject
 
     return composed
   }
+  
+  /**
+   * Creates a copy of an object excluding some keys.
+   * References are kept, so modifing Objects or arrays on the resulting object will modify the source one
+   * To avoid this behaivour clone the input before using or clone the output after
+   * @param {object} o source object to create a copy
+   * @param {string[]} keys Keys to remove
+   * @returns Copy of object without the specified keys
+   * @author Lleonard Subirana (arsu.leo@gmail.com)
+   */
+  composeObjectWithoutKeys(o, ...keys)
+  {
+    const 
+    object = o || {},
+    result = { ...object }
+    for(const key of keys)
+    {
+      delete result[key]
+    }
+    return result
+  }
 }
 
 module.exports = CoreObject
