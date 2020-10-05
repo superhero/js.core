f5ef464c2d64c1dd3fc2b3e276b3decb67efc765
Erik Landvall <erik@landvall.se>
2020-10-05 12:32:52 +0200

    updated request version

---
+++ core/soap/server/index.js
@@ -0,0 +1,18 @@
+class SoapServer
+{
+  constructor(soap)
+  {
+    this.soap = soap
+  }
+
+  listen(server, path, services, wsdl, callback)
+  {
+
+  }
+}
+
+module.exports = SoapServer
+
+
+soap.listen()
+soap.listen(server, '/wsdl', myService, xml)
