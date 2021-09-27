496d781ca4e067136a0304f45a3103e85af0f262
Erik Landvall <erik@landvall.se>
2021-09-27 10:14:38 +0200

    updated the bootstrap process error message

--- core/bootstrap/index.js
+++ core/bootstrap/index.js
@@ -8,17 +8,25 @@ class Bootstrap
   async bootstrap()
   {
     const
-    configuration = this.locator.locate('core/configuration'),
-    bootstrapMap  = configuration.find('core.bootstrap')
+      configuration = this.locator.locate('core/configuration'),
+      bootstrapMap  = configuration.find('core.bootstrap')
 
-    for(const uid in bootstrapMap)
+    for(const key in bootstrapMap)
     {
-      const serviceName = bootstrapMap[uid]
-
-      if(serviceName)
+      try
       {
-        const service = this.locator.locate(serviceName)
+        const 
+          serviceName = bootstrapMap[key],
+          service     = this.locator.locate(serviceName)
+
         await service.bootstrap()
+      }
+      catch(previousError)
+      {
+        const error = new Error('failed to reserve paxad order in legacy layer')
+        error.code  = 'E_CORE_BOOTSTRAP'
+        error.chain = { previousError, key, serviceName, bootstrapMap }
+        throw error
       }
     }
   }
