45032625e5f1bd2cbf8cb10e23f5a1b0688e360e
Erik Landvall <erik@landvall.se>
2024-02-01 13:43:40 +0100

    deepfreze error logs

--- core/deepfreeze/index.js
+++ core/deepfreeze/index.js
@@ -6,11 +6,21 @@ class DeepFreeze
 
     for (const name of propNames)
     {
-      const value = obj[name]
-
-      obj[name] = value && typeof value === 'object'
-      ? this.freeze(value)
-      : value
+      try
+      {
+        const value = obj[name]
+  
+        obj[name] = value && typeof value === 'object'
+        ? this.freeze(value)
+        : value
+      }
+      catch(previousError)
+      {
+        const error = new Error('could not freeze attribute of object')
+        error.code  = 'E_CORE_DEEP_FREEZE'
+        error.chain = { name, propNames, obj, previousError }
+        throw error
+      }
     }
 
     return Object.freeze(obj)
